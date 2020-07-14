a1f46f9aac0140e255a23a2dab6dea2d0182e628
Knoah Cotto <cotto.knoah@gmail.com>
2020-07-14 11:04:34 -0500

    working on servlets

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,25 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
 
-    }
+            String hello = request.getParameter("hello");
+
+            if(hello == null){
+                hello = "Knoah";
+            }
+
+            out.println("<h1> Hello " + hello + "! </h1>");
+
+        }
+
 }
